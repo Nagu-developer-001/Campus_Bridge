04106716471a77b34595b5d7cdf88319b46f538b
Nagu-developer-001 <nagabhoshanabhat@gmail.com>
2026-07-30 21:42:50 +0530

    Commit

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/campusbridge";
-
+console.log("MongoDB URI:", MONGO_URI);
 app.use(cors());
 app.use(express.json());
 
